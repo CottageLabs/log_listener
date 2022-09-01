850ccad5ba23cf8991428cd171766a5edd1fe663
Hrafn Malmquist <hrafn.malmquist@gmail.com>
2022-09-01 16:59:57 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # log_listener
-TCP log listener server
+TCP log listener server minimally adapted from [Vinay Sajip's](https://github.com/vsajip) Python
+[logging cookbook](https://docs.python.org/3/howto/logging-cookbook.html#sending-and-receiving-logging-events-across-a-network).
+
+This is a standalone process, as [described by Vinay](https://docs.python.org/3/howto/logging-cookbook.html#deploying-web-applications-using-gunicorn-and-uwsgi): 
+> "When deploying Web applications using Gunicorn or uWSGI (or similar), multiple worker processes are created to handle
+> client requests. In such environments, avoid creating file-based handlers directly in your web application.
+> Instead, use a SocketHandler to log from the web application to a listener in a separate process. This can be set up
+> using a process management tool such as Supervisor - see Running a logging socket listener in production for more
+> details."
+
+There are several SO posts (see for example [here](https://stackoverflow.com/questions/70141427/django-loggers-are-overwriting-the-previous-log-file-along-with-the-new-one)
+and [here](https://stackoverflow.com/questions/70944237/how-to-log-to-file-using-django-and-gunicorn-using-timedrotatingfilehandler-mis) incidentally answered by Vinay) around this issue, but remarkably,
+Django [does not mention this issue in its documentation](https://docs.djangoproject.com/en/4.1/howto/logging/).
+ 
+`log_listener.py` is a straight copy of Vinay's code for a server listening for log records on a TCP socket.
+
+Log files will be kept in the sub-folder `logs/`.
+
+## Deployment
+A log server requires a process to run it:
+
+`python3 log_listener.py -c pstf_app.ini`
+
+would pass the configuration file `pstf_app.ini` to the server.
+
+this long running process is best served by having it overseen by `supervisor` and `log_listener.conf` is a
+configuration file for `supervisor` to handle two such processes; one for default logging and one for audit logging.
+
+Finally, as log file management has now been decoupled from Django, `log_listener_logrotate.conf` is a configuration
+file for `logrotate` to rotate the log files daily.
---
+++ log_listener.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 Red Dove Consultants Limited. BSD-3-Clause licensed.
+#
+import argparse
+import configparser
+import io
+import logging
+import logging.handlers
+import os
+import pickle
+import socketserver
+import struct
+import sys
+
+PRINT_EXC_TYPE = False
+
+CONFIG_VARS = {
+    'port': '9020',
+    'filename': 'listener.log',
+    'format': '%%(asctime)s %%(name)-15s %%(levelname)-8s %%(message)s',
+    'level': 'DEBUG',
+}
+
+
+class LogRecordStreamHandler(socketserver.StreamRequestHandler):
+    """Handler for a streaming logging request.
+
+    This basically logs the record using whatever logging policy is
+    configured locally.
+    """
+
+    def handle(self):
+        """
+        Handle multiple requests - each expected to be a 4-byte length,
+        followed by the LogRecord in pickle format. Logs the record
+        according to whatever policy is configured locally.
+        """
+        while True:
+            chunk = self.connection.recv(4)
+            if len(chunk) < 4:
+                break
+            slen = struct.unpack('>L', chunk)[0]
+            chunk = self.connection.recv(slen)
+            while len(chunk) < slen:
+                chunk = chunk + self.connection.recv(slen - len(chunk))
+            obj = self.unPickle(chunk)
+            record = logging.makeLogRecord(obj)
+            self.handleLogRecord(record)
+
+    def unPickle(self, data):
+        return pickle.loads(data)
+
+    def handleLogRecord(self, record):
+        # if a name is specified, we use the named logger rather than the one
+        # implied by the record.
+        if self.server.logname is not None:
+            name = self.server.logname
+        else:
+            name = record.name
+        logger = logging.getLogger(name)
+        # N.B. EVERY record gets logged. This is because Logger.handle
+        # is normally called AFTER logger-level filtering. If you want
+        # to do filtering, do it at the client end to save wasting
+        # cycles and network bandwidth!
+        logger.handle(record)
+
+
+class LogRecordSocketReceiver(socketserver.ThreadingTCPServer):
+    """
+    Simple TCP socket-based logging receiver suitable for testing.
+    """
+
+    allow_reuse_address = True
+
+    def __init__(self, host='localhost',
+                 port=logging.handlers.DEFAULT_TCP_LOGGING_PORT,
+                 handler=LogRecordStreamHandler):
+        socketserver.ThreadingTCPServer.__init__(self, (host, port), handler)
+        self.abort = 0
+        self.timeout = 1
+        self.logname = None
+
+    def serve_until_stopped(self):
+        import select
+        abort = 0
+        while not abort:
+            rd, wr, ex = select.select([self.socket.fileno()],
+                                       [], [],
+                                       self.timeout)
+            if rd:
+                self.handle_request()
+            abort = self.abort
+
+
+def main():
+    adhf = argparse.ArgumentDefaultsHelpFormatter
+    parser = argparse.ArgumentParser(formatter_class=adhf)
+    aa = parser.add_argument
+    aa('-c', '--config', help='Configuration file to use')
+    options = parser.parse_args()
+    # print(options)
+    cfg = configparser.ConfigParser(defaults=CONFIG_VARS)
+    fn = options.config
+    if not fn:
+        stream = io.StringIO('[listener]\n')
+    elif not os.path.exists(fn):
+        print(f'Configuration file not found: {fn}')
+        return 1
+    else:
+        stream = open(fn, encoding='utf-8')
+    try:
+        cfg.read_file(stream)
+        port = cfg.getint('listener', 'port')
+        filename = cfg.get('listener', 'filename')
+        fmt = cfg.get('listener', 'format')
+        level = getattr(logging, cfg.get('listener', 'level'))
+        # Replace this next line with however you want to configure local logging
+        # (e.g. with a RotatingFileHandler rather than a FileHandler)
+        logging.basicConfig(level=level, format=fmt, filename=filename)
+        logging.getLogger().info('Log listener started.')
+        tcpserver = LogRecordSocketReceiver(port=port)
+        print(f'About to start TCP server on port {port} ...')
+        tcpserver.serve_until_stopped()
+    finally:
+        stream.close()
+
+
+if __name__ == '__main__':
+    try:
+        rc = main()
+    except KeyboardInterrupt:
+        rc = 2
+    except Exception as e:
+        if PRINT_EXC_TYPE:
+            s = ' %s:' % type(e).__name__
+        else:
+            s = ''
+        sys.stderr.write('Failed:%s %s\n' % (s, e))
+        if 'PY_DEBUG' in os.environ:
+            import traceback
+            traceback.print_exc()
+        rc = 1
+    sys.exit(rc)
