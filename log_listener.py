617e1357c6b12dc1ed6f021b25215cd44182d213
Hrafn Malmquist <hrafn.malmquist@gmail.com>
2022-09-21 16:11:40 +0100

    Remove debug line

--- log_listener.py
+++ log_listener.py
@@ -102,7 +102,6 @@ def main():
     # (e.g. with a RotatingFileHandler rather than a FileHandler)
     logging.basicConfig(level=options.level, format=options.format, filename=options.filename)
     logging.getLogger().info('Log listener started.')
-    logging.getLogger().info('More stuff.')
     tcpserver = LogRecordSocketReceiver(port=int(options.port))
     print(f'Starting TCP server on port {options.port} ...')
     tcpserver.serve_until_stopped()
